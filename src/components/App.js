90cda128f847e139799c818cec95dd79d00248af
Roman Shatskikh <shatskikh.roman@gmail.com>
2022-07-24 12:47:39 +0300

    fix: add popup for error of login

--- src/components/App.js
+++ src/components/App.js
@@ -66,6 +66,8 @@ export default function App() {
                 }
             })
             .catch(err => {
+                setAuthStatus(false);
+                setIsAuthStatusPopupOpen(true);
                 console.error(`Ошибка: ${err}`);
             });
     }
